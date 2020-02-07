ab2d025ec3a04f99c4535279e7f5e3a3865295ed
Dion Garman <diongarman@gmail.com>
2020-02-07 23:22:27 +0000

    can define functions into interface

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,19 +1,21 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'Civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name is ${this.name}`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Name: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
