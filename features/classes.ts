3200591f1743941901c6d6e56f3092d3b1aa1613
Dion Garman <diongarman@gmail.com>
2020-02-08 17:17:18 +0000

    class modifiers - protected

--- features/classes.ts
+++ features/classes.ts
@@ -1,8 +1,16 @@
 class Vehicle {
-  public honk(): void {
+  colour: string;
+  constructor(colour: string) {
+    this.colour = colour;
+  }
+
+  protected honk(): void {
     console.log('beep');
   }
 }
+
+const vehicle = new Vehicle('Orange');
+console.log(vehicle.colour);
 
 class Car extends Vehicle {
   private drive(): void {
@@ -11,9 +19,10 @@ class Car extends Vehicle {
 
   startDrivingProcess(): void {
     this.drive();
+    this.honk();
   }
 }
 
-const car = new Car();
+const car = new Car('Black');
 car.startDrivingProcess();
-car.honk();
+//car.honk();
